fc981511d1ff58983a75e35f26b4c48c57adab4d
adelinesharla <adelinesharla@duck.com>
2025-02-11 10:03:26 -0300

    fix: uri

--- modules/api/main.tf
+++ modules/api/main.tf
@@ -39,7 +39,7 @@ resource "aws_api_gateway_integration" "upload" {
   http_method             = aws_api_gateway_method.upload.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = "arn:aws:apigateway:${var.aws_region}/lambda/path/2015-03-31/functions/${var.upload_lambda_arn}/invocations"
+  uri                     = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/${var.upload_lambda_arn}/invocations"
 }
 
 # Method Response for Upload
