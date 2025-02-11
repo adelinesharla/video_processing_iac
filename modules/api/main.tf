6e67be50be267b173f9a94baee4567e3dca4ae74
adelinesharla <adelinesharla@duck.com>
2025-02-11 00:22:24 -0300

    fix: api

--- modules/api/main.tf
+++ modules/api/main.tf
@@ -39,7 +39,27 @@ resource "aws_api_gateway_integration" "upload" {
   http_method             = aws_api_gateway_method.upload.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = var.upload_lambda_arn
+  uri                     = "arn:aws:apigateway:${var.aws_region}/lambda/path/2015-03-31/functions/${var.upload_lambda_arn}/invocations"
+}
+
+# Method Response for Upload
+resource "aws_api_gateway_method_response" "upload" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  resource_id = aws_api_gateway_resource.videos.id
+  http_method = aws_api_gateway_method.upload.http_method
+  status_code = "200"
+}
+
+# Integration Response for Upload
+resource "aws_api_gateway_integration_response" "upload" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  resource_id = aws_api_gateway_resource.videos.id
+  http_method = aws_api_gateway_method.upload.http_method
+  status_code = aws_api_gateway_method_response.upload.status_code
+
+  depends_on = [
+    aws_api_gateway_integration.upload
+  ]
 }
 
 # CORS Configuration
